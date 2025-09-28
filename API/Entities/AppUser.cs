110a869ff973fba32b7efa5daecb5cca05329f47
Konesan <konesan04@gmail.com>
2025-09-28 11:08:02 +0530

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+namespace API.Entities;
+
+public class AppUser
+{
+    public required string Id { get; set; }
+    public required string DisplayName { get; set; }
+    public required string Email { get; set; }
+}
