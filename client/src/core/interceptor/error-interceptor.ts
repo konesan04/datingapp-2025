5dad5c2f3dcbf1b26c40df5f27ef8c5590701383
Konesan <konesan04@gmail.com>
2025-10-09 14:59:02 +0530

    End of section 7

---
+++ client/src/core/interceptor/error-interceptor.ts
@@ -0,0 +1,43 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { ToastService } from '../services/toast-service';
+import { NavigationExtras, Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const toast = inject(ToastService);
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError(error => {
+      if (error) {
+        switch (error.status) {
+          case 400:
+            if (error.error.errors) {
+              const modelStateErrors = [];
+              for (const key in error.error.errors) {
+                modelStateErrors.push(error.error.errors[key])
+              }
+              throw modelStateErrors.flat()
+            } else {
+              toast.error(error.error)
+            }
+            break;
+          case 401:
+            toast.error('Unauthorized');
+            break;
+          case 404:
+            router.navigateByUrl('/not-found')
+            break;
+          case 500:
+            const navigationExtras: NavigationExtras = {state: {error:error.error}}
+            router.navigateByUrl('/server-error', navigationExtras)
+            break;
+          default:
+            toast.error('Something went wrong');
+        }
+      }
+      throw error;
+    })
+  )
+};
