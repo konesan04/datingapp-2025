16fc749123c02b47a4f9f4638a503cbb8da4140c
Konesan <konesan04@gmail.com>
2025-10-16 11:51:15 +0530

    End of section 10

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Member, Photo } from '../../types/members';
+import { editableMember, Member, Photo } from '../../types/members';
 import { AccountService } from './account-service';
+import { tap } from 'rxjs';
 
 
 @Injectable({
@@ -12,17 +13,29 @@ export class MemberService {
   private http = inject(HttpClient);
   private accountService = inject(AccountService);
   private baseUrl = environment.apiUrl;
+  editMode = signal(false)
+  member = signal<Member | null>(null)
+
 
   getMembers() {
     return this.http.get<Member[]>(this.baseUrl + 'members');
   }
 
   getMember(id: string) {
-    return this.http.get<Member>(this.baseUrl + 'members/'+ id)
+    return this.http.get<Member>(this.baseUrl + 'members/'+ id).pipe(
+      tap(member => {
+        this.member.set(member)
+      })
+    )
   }
   
   getMemberPhotos(id: string) {
     return this.http.get<Photo[]>(this.baseUrl + 'members/' +id +'/photos')
+  }
+
+  updateMember(member: editableMember) {
+    return this.http.put(this.baseUrl + 'members/', member)
+
   }
 
   
